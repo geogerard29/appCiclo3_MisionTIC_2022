ab77e662f60b301e7126c47e3a0bb3b43605c6ad
Dagh1981 <dagh81.dg@gmail.com>
2022-08-26 10:40:58 -0400

    Modelado clase empresa y empleado

--- proyectoCicloIII/src/main/java/clasesModeladas/Empleado.java
+++ proyectoCicloIII/src/main/java/clasesModeladas/Empleado.java
@@ -3,14 +3,63 @@ package clasesModeladas;
 public class Empleado {
 
     // Agregar los atributos del empleado:  nombre, correo, empresa (a la que pertenece el empleado) y rol (administrador u operativo)
-
-
+    int id;
+    String name;
+    String email;
+    String enterprise;
+    String role;
 
 
     // Agregar metodo constructor
 
+    public Empleado(int id, String name, String email, String enterprise, String role)  {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.enterprise = enterprise;
+        this.role = role;
 
-
+    }
 
     // Agregar getters y setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(String enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- proyectoCicloIII/src/main/java/clasesModeladas/Empresa.java
+++ proyectoCicloIII/src/main/java/clasesModeladas/Empresa.java
@@ -1,16 +1,60 @@
 package clasesModeladas;
-
 public class Empresa {
 
     // Agregar los atributos de la Empresa:  nombre, direcciòn, telefono y nit
-
-
-
+    String name;
+    String nit;
+    int phone;
+    String address;
+    int id;
 
     // Agregar metodo constructor
-
-
-
-
+    public Empresa(int id,String name, String nit, int phone, String address) {
+        this.name = name;
+        this.nit = nit;
+        this.phone = phone;
+        this.address = address;
+        this.id = id;
+    }
     // Agregar getters y setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
