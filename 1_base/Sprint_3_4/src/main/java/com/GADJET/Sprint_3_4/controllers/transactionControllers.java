fccfa6d69c489fd8c81acc3c2ec79deea1065f14
Dagh1981 <dagh81.dg@gmail.com>
2022-09-03 13:53:08 -0400

    Se modifica Clase, Controlador, Entidad, Repositorio Transactions

--- 1_base/Sprint_3_4/src/main/java/com/GADJET/Sprint_3_4/controllers/transactionControllers.java
+++ 1_base/Sprint_3_4/src/main/java/com/GADJET/Sprint_3_4/controllers/transactionControllers.java
@@ -1,4 +1,42 @@
 package com.GADJET.Sprint_3_4.controllers;
 
+import com.GADJET.Sprint_3_4.entities.Transaction;
+import com.GADJET.Sprint_3_4.repositories.transactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("transactions")
 public class transactionControllers {
+
+    @Autowired
+    private transactionRepository transactionsRepository;
+    @GetMapping()
+    // Metodo para construir todos las transacciones
+    public ResponseEntity<List<Transaction>> getTransaction(){
+        List<Transaction> transactions = transactionsRepository.findAll();
+
+        return ResponseEntity.ok(transactions);
+    }
+
+    // Metodo para listar una transaccion por su identificacion
+    /*@RequestMapping(value = "{productId}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable("TransactionId") Long TransactionId){
+        java.util.Optional<Transaction> optionalTransaction = transaction.findById(TransactionId);
+        if (((java.util.Optional<Transaction>) optionalTransaction).isPresent()) {
+            return ResponseEntity.ok(((java.util.Optional<Transaction>) optionalTransaction).get());
+        }
+        else
+            return ResponseEntity.notFound().build();
+    }*/
+
+    // Metodo para generar un nueva transaccion
+    /*@PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transactionn){
+        Transaction newtransactionn = transaction.save(transactionn);
+        return ResponseEntity.ok(newtransactionn);
+    }*/
 }
--- 1_base/Sprint_3_4/src/main/java/com/GADJET/Sprint_3_4/entities/Transaction.java
+++ 1_base/Sprint_3_4/src/main/java/com/GADJET/Sprint_3_4/entities/Transaction.java
@@ -1,4 +1,80 @@
 package com.GADJET.Sprint_3_4.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Transactions")
 public class Transaction {
+
+    //Atributos
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "Amount")
+    private double amount;
+    @Column(name = "Employee")
+    private String employee;
+    @Column(name = "Concept")
+    private String concept;
+    private enum typeTran{
+        Incomes,
+        Expenses
+    }
+    @Column(name = "Type of Transaction")
+    typeTran type;
+
+    //Constructor vacio
+    public Transaction() {
+    }
+
+    //Constructor con parametros
+    public Transaction(long id, Double amount, String employee, String concept, typeTran type) {
+        this.id = id;
+        this.amount = amount;
+        this.employee = employee;
+        this.concept = concept;
+        this.type = type;
+    }
+
+    //Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public typeTran getType() {
+        return type;
+    }
+
+    public void setType(typeTran type) {
+        this.type = type;
+    }
 }
